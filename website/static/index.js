7c88186a4c1807536102712ca151d52cf3dddce7
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-05-23 19:34:58 +0100

    Update index.js

--- website/static/index.js
+++ website/static/index.js
@@ -1,40 +1,59 @@
-// navigate to home page,article page, topic page
+// navigate to home page, article page, topic page
 
+/**
+ * Navigates to the home page.
+ */
 function goToHome() {
-    window.location.replace(window.location.origin)
+    window.location.replace(window.location.origin);
 }
 
+/**
+ * Navigates to a specific topic page.
+ * @param {string} topic - The topic to navigate to.
+ */
 function goTo(topic) {
-    window.location.replace(window.location.origin.concat("/topics/").concat(topic));
+    window.location.replace(`${window.location.origin}/topics/${topic}`);
 }
 
+/**
+ * Navigates to a specific article page.
+ * @param {string} article - The article to navigate to.
+ */
 function goToArticle(article) {
-    window.location.replace(window.location.origin.concat("/topics/article/").concat(article))
+    window.location.replace(`${window.location.origin}/topics/article/${article}`);
 }
 
 
-// open,close navigation bar
+// open, close navigation bar
 
+/**
+ * Toggles the navigation bar.
+ */
 function change_navbar() {
-    x = document.getElementById('navbar_list_button');
+    var x = document.getElementById('navbar_list_button');
     x.classList.toggle("change");
-    target = document.getElementById("navbar_list")
-    if (target.style.display == 'none') {
-        target.style.display = 'inline-block'
+    var target = document.getElementById("navbar_list");
+    if (target.style.display === 'none') {
+        target.style.display = 'inline-block';
     } else {
-        target.style.display = 'none'
-    };
+        target.style.display = 'none';
+    }
 }
 
 
 // sort & search functions
 var timer = null;
+
 // topics
 
+/**
+ * Handles the keyup event for the topic search filter.
+ */
 $("#search_filter_topics").keyup(function() {
     var text = $(this).val();
     var state = $('#state_topics').val();
     clearTimeout(timer);
+
     var req;
 
     var inner_function = function() {
@@ -53,30 +72,36 @@ $("#search_filter_topics").keyup(function() {
                 req = null;
             }
         });
-    }
+    };
 
     timer = setTimeout(inner_function, 100);
 });
 
+/**
+ * Handles the click and mouseleave events for the topic sorting.
+ */
 $('#sort_topics').on('click mouseleave', function(event) {
     var state_change = document.getElementById('state_change_topics');
-    if (event.type == 'mouseleave') {
+    if (event.type === 'mouseleave') {
         state_change.value = 0;
     } else {
         state_change.value = 1;
         var form = document.getElementById('topics_sort_search');
         form.submit();
     }
-
 });
 
-//categories
+// categories
 
+/**
+ * Handles the keyup event for the article search filter.
+ */
 $("#search_filter_article").keyup(function() {
     var slug = $("#category_identifier").val();
     var text = $(this).val();
     var state = $("#state_article").val();
     clearTimeout(timer);
+
     var req;
 
     var inner_function = function() {
@@ -84,7 +109,7 @@ $("#search_filter_article").keyup(function() {
             req.abort();
         }
         req = $.ajax({
-            url: '/topics/'.concat(slug),
+            url: `/topics/${slug}`,
             type: 'get',
             data: {
                 jsdata: text,
@@ -95,31 +120,37 @@ $("#search_filter_article").keyup(function() {
                 req = null;
             }
         });
-    }
+    };
 
     timer = setTimeout(inner_function, 100);
 });
 
+/**
+ * Handles the click and mouseleave events for the article sorting.
+ */
 $('#sort_article').on('click mouseleave', function(event) {
     var state_change = document.getElementById('state_change_articles');
-    if (event.type == 'mouseleave') {
+    if (event.type === 'mouseleave') {
         state_change.value = 0;
     } else {
         state_change.value = 1;
         var form = document.getElementById('articles_sort_search');
         form.submit();
     }
-
 });
 
 
+// flash messages
 
-//  flash messages
+/**
+ * Closes the flash alert message.
+ */
 function close_alert() {
     var alert = document.getElementById("alert");
     alert.style.display = 'none';
 }
 
+// Hides the flash alert message after 3000 milliseconds (3 seconds).
 window.setTimeout(function() {
     $('#alert').fadeOut('slow');
 }, 3000);
