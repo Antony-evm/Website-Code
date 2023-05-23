1f3fa5446cc207c93a433751f0602a454752c75f
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-05-23 15:49:05 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,20 +1,19 @@
 from website import create_app
 from flask import send_from_directory, request
+from flask.wrappers import Response
 
 
 application = create_app()
 
 
-@application.route("/")
-def index():
-    pass
-
-
 @application.route("/robots.txt")
 @application.route("/sitemap.xml")
-def static_from_root():
+def static_from_root() -> Response:
+    """
+    Outputs the sitemap and the robots files from the static folder
+    """
     return send_from_directory(application.static_folder, request.path[1:])
 
 
 if __name__ == "__main__":
-    application.run(debug=False)
+    application.run(debug=True)
