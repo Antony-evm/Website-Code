cc6ac779b8a3eacaffc5888374844fa842dc8d8a
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-05-23 15:49:26 +0100

    Update articles.py

--- website/articles.py
+++ website/articles.py
@@ -5,7 +5,11 @@ articles = Blueprint("articles", __name__)
 
 
 @articles.route("/<cat_slug>/<article_slug>", methods=["GET"])
-def go_to_article(cat_slug, article_slug):
+def go_to_article(cat_slug: str, article_slug: str) -> str:
+    """
+    This function is responsible for finding the correct article to display.
+    Uses cat_slug and article_slug to find the article within the database and returns the specified template.
+    """
     category = Categories.query.filter_by(slug=cat_slug).first()
     article = Articles.query.filter_by(slug=article_slug).first()
 
